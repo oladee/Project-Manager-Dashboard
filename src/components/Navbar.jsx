93b4a873379e5e2b048e1531b7899bc121a7c6f3
oladee <deemajor230600@gmail.com>
2023-08-17 18:15:40 +0100

    added conditional for arrow flip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className='w-[75%]'>
         <nav className='flex items-center justify-between px-4 py-2 fixed top-0 right-0 bg-white w-full md:w-[80%]'>
             <div className='flex items-center'>
-            <img className='w-32 md:hidden' src={hamburger} alt="hamburger menu" />
+            <img className='md:hidden' src={hamburger} alt="hamburger menu" />
             <img src={sailLogo} className='w-32 md:hidden' alt="Logo icon" />
             </div>
             <div className='flex items-center'>
--- src/components/Sidepane.jsx
+++ src/components/Sidepane.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react'
 import hamburger from  '../assets/icon-menu.svg'
 import sailLogo from '../assets/Sail logo.png'
 
 const Sidepane = () => {
   return (
     <div className='hidden md:block w-[25%] bg-white overflow-y-scroll fixed top-0 left-0 z-10 max-h-[100vh]'>
-      <nav className='flex items-center justify-between px-4 py-2'>
-        <img className='w-32' src={sailLogo} alt="sail logo" />
+      <nav className='flex items-center justify-between px-2 py-2'>
+        <img className='w-28' src={sailLogo} alt="sail logo" />
         <img src={hamburger} alt="hamburger icon" />
       </nav>
       <ul>
-
+        <Sidepanecontent heading='Dashboard' arrow={true}/>
+        <Sidepanecontent/>
+        <Sidepanecontent/>
       </ul>
     </div>
   )
@@ -20,13 +23,19 @@ export default Sidepane
 
 const Sidepanecontent = ({heading,icon, arrow}) => {
 
-  return(<div>
+  const [direction, setDirection] = useState(false)
+
+  function directionHandler(){
+    setDirection(!direction)
+  }
+
+  return(<div onClick={directionHandler}>
     <div className="sideIcon">
-      <img src="" alt="" />
+      <img src={icon} alt="" />
     </div>
     <div className="sideContent">
-      <h3></h3>
-      {arrow && <img src='' alt=''/>}
+      <h3>{heading}</h3>
+      {arrow && <img src={direction? sailLogo : hamburger} alt=''/>}
     </div>
   </div>)
 }
